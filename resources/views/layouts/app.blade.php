e503db3a95587cd3ad987183d5e23ed6b8d74781
Mohamed Torkey <120189395+mohamedtorkey20@users.noreply.github.com>
2024-04-01 00:55:16 +0200

    modfiy style of profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,9 @@
                     @include('layouts.navbar')
                 </div>
             @endauth
-            <div class="app">
+
                 @yield('body')
-            </div>
+           
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
