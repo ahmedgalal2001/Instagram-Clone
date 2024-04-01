3533ecc5f0bdecf6dfad67a075bc6988b6b119c9
Mohamed Torkey <120189395+mohamedtorkey20@users.noreply.github.com>
2024-04-02 01:02:34 +0200

    modified

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,25 +20,8 @@
                     @include('layouts.navbar')
                 </div>
             @endauth
-
-
                 @yield('body')
-<<<<<<< Updated upstream
-           
-<<<<<<< HEAD
-=======
-=======
->>>>>>> refs/remotes/origin/mohamedtorky/profile
 
-
-
-
-<<<<<<< HEAD
-
->>>>>>> Stashed changes
-=======
->>>>>>> refs/remotes/origin/mohamedtorky/profile
-        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
