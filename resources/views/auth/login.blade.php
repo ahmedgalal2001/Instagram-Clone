27ea545e34e4372b76e56778c07788a3b25d28a8
Ahmed Mansour <ahmed.mansour10257@gmail.com>
2024-04-02 21:41:38 +0200

    register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,53 @@
 @extends('layouts.app')
 @section('title', 'User')
 @section('body')
-@vite(['resources/css/login.css'])
-<div class="col-12 d-flex justify-content-center align-items-center " id="login">
-    <div class="col-6 img-fluid text-center">
-        <img src={{ asset('images/login.png') }} alt="LoginPage" >
+    @vite(['resources/css/login.css'])
+    <div class="col-12 d-flex justify-content-center align-items-center " id="login">
+        <div class="col-6 img-fluid text-center">
+            <img class="img-fluid"src={{ asset('images/login.png') }} alt="LoginPage">
+        </div>
+        <div class="col-4 ">
+            <div class="border p-5">
+                <div class="text-center mb-5 ">
+                    <h1>Instagram</h1>
+                </div>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="mb-1 ">
+                        <input type="email" name="email" class="w-100 formborder form-control" id="exampleInputEmail1"
+                            aria-describedby="emailHelp" placeholder="Phone number,username, or email">
+                    </div>
+                    <div class="mb-3 ">
+                        <input type="password" name="password" class="w-100 form-control" id="exampleInputPassword1"
+                            placeholder="password">
+                    </div>
+                    <div class=" mb-3">
+                        <button type="submit" class="btn lightblue w-100 text-light">Login</button>
+                    </div>
+                    <div>
+                        <hr
+                            style='border: none; border-bottom: 1px solid black; width: 40%; display: inline-block; margin: 0 10px;'>
+                        OR
+                        <hr
+                            style='border: none; border-bottom: 1px solid black; width: 40%; display: inline-block; margin: 0 10px;'>
+                    </div>
+                    <p class="text-center blue mt-3"><i class="fa-brands fa-square-facebook blue"></i> Log in with Facebook
+                    </p>
+                    <p class="text-center blue">forget password?</p>
+                </form>
+            </div>
+            <div class="border mt-3 w-100 p-3 text-center">
+                <p>Donot have an account? <a href="">Sign up</a></p>
+            </div>
+            <div class="text-center mt-3">
+                <p>Get the app.</p>
+            </div>
+            <div class="text-center">
+                <img class="me-3" src={{ asset('images/googleplay.png') }} alt="googleplay" width="120px" height="40px">
+                <img src={{ asset('images/microsoftstore.png') }} alt="microsoftstore" width="120px" height="40px">
+            </div>
+        </div>
+        <div class="col-2"></div>
+
     </div>
-    <div class="col-4 ">
-        <div class="border p-5">
-        <div class="text-center mb-5 ">
-            <h1>Instagram</h1>
-        </div>
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <div class="mb-1 ">
-                <input type="email" name="email" class="w-100 formborder form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Phone number,username, or email">
-            </div>
-            <div class="mb-3 ">
-                <input type="password" name="password" class="w-100 form-control" id="exampleInputPassword1" placeholder="password">
-            </div>
-            <div class=" mb-3">
-                <button type="submit" class="btn lightblue w-100 text-light">Login</button>
-            </div>
-            <div>
-                <hr style='border: none; border-bottom: 1px solid black; width: 40%; display: inline-block; margin: 0 10px;'>
-                OR
-                <hr style='border: none; border-bottom: 1px solid black; width: 40%; display: inline-block; margin: 0 10px;'>
-            </div>
-            <p class="text-center blue mt-3"><i class="fa-brands fa-square-facebook blue"></i> Log in with Facebook</p>
-            <p class="text-center blue">forget password?</p>
-        </form>
-    </div>
-        <div class="border mt-3 w-100 p-3 text-center">
-            <p>Donot have an account? <a href="">Sign up</a></p>
-        </div>
-        <div class="text-center mt-3">
-            <p>Get the app.</p>
-        </div>
-        <div class="text-center">
-            <img class="me-3" src={{ asset('images/googleplay.png') }} alt="googleplay" width="120px" height="40px"  >
-            <img  src={{ asset('images/microsoftstore.png') }} alt="microsoftstore" width="120px" height="40px"  >
-        </div>
-    </div>
-    <div class="col-2"></div>
-    
-</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,28 +1,52 @@
 @extends('layouts.app')
 @section('title', 'User')
 @section('body')
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" name="name" class="form-control" id="name">
+    <div class="conatiner p-5">
+        <div class="card card-body rounded-0 m-5 p-5 text-center">
+            <div class="row d-flex flex-column">
+                <div>
+                    <a href="{{ route('login') }}"><img
+                            class="img-fluid w-50 center-block"src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
+                            alt=""></a>
+                </div>
+                <div class="h6 text-center">Sign up to see photos and videos from your friends.</div>
+                <button type="submit" class="btn btn-primary w-100 fa-facebook">Log in with Facebook</button>
+            </div>
+            <p>-----------OR--------------</p>
+            <div class="row">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="mb-3 ">
+
+                        <input type="email" name="email" class="form-control rounded-0" id="email"
+                            placeholder="Mobile Number or Email">
+                    </div>
+                    <div class="mb-3 ">
+
+                        <input type="text" name="name" class="form-control rounded-0" id="name"
+                            placeholder="Full Name">
+                    </div>
+                    <div class="mb-3 ">
+
+                        <input type="text" name="name" class="form-control rounded-0" id="name"
+                            placeholder="UserName">
+                    </div>
+
+                    <div class="mb-3 ">
+
+                        <input type="password" name="password" class="form-control rounded-0" id="password"
+                            placeholder="Password">
+                    </div>
+                    <p>People who use our service may have uploaded your contact information to Instagram. Learn More</p>
+                    <p>By signing up, you agree to our Terms , <a href="">Privacy Policy</a> and <a
+                            href="">Cookies Policy .</a></p>
+                    <button type="submit" class="btn btn-primary w-100">Sign up</button>
+                </form>
+            </div>
+
         </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email address</label>
-            <input type="email" name="email" class="form-control" id="email">
+        <div class="card card-body rounded-0 m-5 p-5 text-center">
+            <p>Have an account? <a href="{{ route('login') }}">Log in</a></p>
         </div>
-        <div class="mb-3">
-            <label for="password_confirmation" class="form-label">Confirmation Password</label>
-            <input type="password" name="password_confirmation" class="form-control" id="password_confirmation">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" id="password">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
 @endsection
