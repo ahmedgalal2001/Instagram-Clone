3f49f4d9d488787b015588f08e602835d13addbc
ahmedgalal2001 <lil>
2024-04-08 01:14:45 +0200

    update Register User

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,7 +43,6 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        dd($user);
 
         event(new Registered($user));
 
--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Mail\EmailVerification;
 use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
@@ -12,15 +13,16 @@ class MailController extends Controller
 {
     public function sendMsg(string $email)
     {
-        $user = User::where("email", $email)->firstOrFail();
-        if($user){
+        try {
+            $user = User::where('email', $email)->firstOrFail();
             dd($user);
+            $randomCode = Str::random(10);
+            Mail::to($email)->send(new EmailVerification($randomCode));
+        } catch (ModelNotFoundException $e) {
+            dd($e);
         }
-        $randomCode = Str::random(10);
-        Mail::to($email)->send(new EmailVerification($randomCode));
     }
     public function verificationCode($email)
     {
-
     }
 }
